fa7293bc3e1b1e16d1bbfbcc760c41cb955092d0
ErblinSylejmani <erblinsylejmani6@gmail.com>
2026-01-26 18:15:38 +0100

    Shtova nje footer

---
+++ footer.php
@@ -0,0 +1,4 @@
+<p class="text-center text-white bg-dark p-3 mt-3">Copyright &copy; 2026, All Right Reserved</p>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+</html>
