bce1441b0dc942ae5f2add17acb3d203b1e8ea1d
ErblinSylejmani <erblinsylejmani6@gmail.com>
2026-01-26 18:14:57 +0100

    Shtova navbar-in

---
+++ header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ditari</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand text-white" href="#">Ditari</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link text-white" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="#">Nxenesit</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="#">Shto Nxenesit</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="#">Notat</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="#">Shto Notat</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="#">Mungesat</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="#">Shto Mungesat</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="#">Raporti</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
