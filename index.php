1c7c07a52315e484d91720140a839d5eb9f3cd50
ErblinSylejmani <erblinsylejmani6@gmail.com>
2026-01-26 18:19:41 +0100

    Shtova home page

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+include('header.php');
+
+?>
+
+<div class="container text-center mt-5 justify-content-center" style="min-height: 90vh;">
+    <h1 class="display-1">Miresevini ne Platformen tone</h1>
+
+    <a class="btn btn-dark mt-3">Shko te Nxënësit</a>
+</div>
+
+<?php
+
+include('footer.php');
+
+?>
