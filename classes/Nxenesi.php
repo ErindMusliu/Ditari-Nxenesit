1c42f9cb96ccec05c129f2c1cfd1bfc05d239bb3
ErblinSylejmani <erblinsylejmani6@gmail.com>
2026-01-26 18:28:02 +0100

    Shtova disa metoda

---
+++ classes/Nxenesi.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once('Database.php');
+
+class Nxenesi extends Database{
+    public function index(){
+        $sql = "select * from nxenesit";
+
+        return $this->conn->query($sql);
+    }
+
+    public function find(int $id){
+        $sql = "select * from nxenesit where id='$id'";
+        
+        return $this->conn->query($sql);
+    }
+
+    public function shto_nxenesin(string $emri, string $mbiemri, string $klasa){
+        $sql = "insert into nxenesit(shto_nxenesin(emri,mbiemri,klasa) value('$emri','$mbiemri','$klasa')";
+
+        return $this->conn->query($sql);
+    }
+
+    public function perditeso_nxenesin(int $id, string $emri, string $mbiemri, string $klasa){
+        $sql = "update nxenesit set emri='$emri',mbiemri='$mbiemri',klasa='$klasa' where id='$id'";
+
+        return $this->conn->query($sql);
+    }
+
+    public function fshi_nxenesin(int $id){
+        $sql = "delete from nxenesit where id='$id'";
+
+        return $this->conn->query($sql);
+    }
+}
